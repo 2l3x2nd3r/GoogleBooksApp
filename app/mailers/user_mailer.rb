975b6b490e50264c3f71d46ac5b33b1ec2630590
Alexander Taborda <alextab93@gmail.com>
2015-05-21 04:36:25 -0500

    confirmation route change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,12 @@ class UserMailer < ApplicationMailer
   default from: "no-reply@developmentteam.com"
 
  def activation_needed_email(user)
-    @user, @url = user, "piratebooks.herokuapp.com/confirmations/#{user.activation_token}"
+    @user, @url = user, "pickappbook.herokuapp.com/confirmations/#{user.activation_token}"
     mail to: user.email, subject: 'Sucessfully register'
   end
 
    def reset_password_email(user)
-    @user, @url = user, "piratebooks.herokuapp.com/password_resets/#{user.reset_password_token}/edit"
+    @user, @url = user, "pickappbook.herokuapp.com/password_resets/#{user.reset_password_token}/edit"
     mail to: user.email, subject: 'Recover password'
   end  
 end
