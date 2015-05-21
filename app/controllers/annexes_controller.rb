5529c56bb07948ddacf271a758fa741e662b8608
Alexander Taborda <alextab93@gmail.com>
2015-05-21 14:42:34 -0500

    change image of dbaines48

--- app/controllers/annexes_controller.rb
+++ app/controllers/annexes_controller.rb
@@ -11,6 +11,6 @@ class AnnexesController < ApplicationController
     @contacts = {}
     @contacts[0] = {name: 'Alexander Taborda', username: 'x2l3x2nd3rx', celphone: '3004348412', image: 'http://i62.tinypic.com/314tu6t.jpg'}
     @contacts[1] = {name: 'Fabian Betancur', username: 'faanbece',celphone: '3007249114', image: 'http://i60.tinypic.com/21nh9ih.jpg'}
-    @contacts[2] = {name: 'Daniel Blanco', username: 'dbaines48',celphone: '3003392676', image: 'http://i59.tinypic.com/2ui9h1k.jpg'} 
+    @contacts[2] = {name: 'Daniel Blanco', username: 'dbaines48',celphone: '3003392676', image: 'http://i62.tinypic.com/5y6fdi.jpg'} 
   end
 end
