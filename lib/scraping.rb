455925594c6367b8edfcff89ee62b47a64f8d42f
Alexander Taborda Acosta <alextab93@gmail.com>
2015-10-18 16:56:11 -0500

    adding scraping

--- lib/scraping.rb
+++ lib/scraping.rb
@@ -3,12 +3,14 @@ module Scraping
 
   def self.scrap
     bot = Mechanize.new { |a| a.ssl_version, a.verify_mode = 'TLSv1', OpenSSL::SSL::VERIFY_NONE }
+    bot.request_headers["accept-language"] = "es-ES,es;q=0.8,en;q=0.6"
 
     page = 'https://play.google.com/store/books/collection/topselling_paid'
     response = bot.get(page).parser
-    links = response.css('div.card-content a:nth-child(1)')
+
+    links = response.css('div.card-content a.card-click-target')
     images = response.css('div.card-content img')
-    names = response.css('div.details a')
+    names = response.css('div.details a.title')
     info = {}
     
     (0..9).each do |i|
